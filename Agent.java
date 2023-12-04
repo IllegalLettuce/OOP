f563ab20edde86d82f3de40e988c53072de905ee
Anthony Sabbe <100137579+IllegalLettuce@users.noreply.github.com>
2023-12-04 09:34:09 +0000

    Superclass Agent

---
+++ Agent.java
@@ -0,0 +1,32 @@
+/**
+ * Superclass
+ */
+public class Agent {
+
+    //Fields
+    private final int agentID;
+
+    /**Constructor
+     * Creates one instance of the Agent object
+     * @param AgentID - the ID of the object 
+     */
+    public Agent(int agentID){
+        this.agentID = agentID;
+    }
+
+    /**Get method
+     * Returns the ID of the Agent 
+     * @return The ID of the Agent
+     */
+    public final int getID(){
+        return this.agentID;
+    }
+
+    /** Other Methods
+     * @param other
+     * @return if 'this' and 'other' is equal
+     */
+    public boolean equals(Agent other) {
+        return this.agentID == other.agentID;
+    }
+}
